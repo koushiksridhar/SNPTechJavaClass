ff8397a56d5166905fce0ff10fc0c55f08935a71
koushiksridhar <koushiksridhar02@gmail.com>
2020-06-25 17:59:42 -0400

    add HelloWorld

---
+++ src/com/azureworks/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.azureworks.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        //This code is our first Java program
+        //The code will put "Hello World" on the console
+
+        System.out.println("Hello World");
+
+        //System.out.print statement prints the text inside the parenthesis to the console
+
+    }
+}
